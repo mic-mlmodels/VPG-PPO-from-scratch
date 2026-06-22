b34e2ca8e9d8406ededc4dedcb3928ec6230f4ea
Michael <michaelzyx1@gmail.com>
2026-06-22 22:30:58 +1000

    more fails lmao

--- PPO.py
+++ PPO.py
@@ -13,6 +13,7 @@ ACT_DIM = env.action_space.n  # type: ignore
 HIDDEN_DIM = 64
 EPISODE_NUM = 5000
 DISCOUNT = 0.99
+EPSILON = 0.2
 
 
 class PolicyNN(nn.Module):
@@ -54,44 +55,62 @@ for episode in range(EPISODE_NUM):
         param.requires_grad = False
     policy_optimiser.zero_grad()
     value_optimiser.zero_grad()
-    states_lst = []
-    actions_lst = []
-    rewards_lst = []
-    log_probs_lst = []
-    future_rewards_lst = []
-    state_value_lst = []
-    np_state, info = env.reset()
+    old_states_lst = []
+    old_actions_lst = []
+    old_rewards_lst = []
+    old_log_probs_lst = []
+    old_future_rewards_lst = []
+    old_state_value_lst = []
+    old_np_state, info = env.reset()
     if episode % 100 == 0:
         print(episode)
     while True:
-        torch_state = torch.as_tensor(np_state, dtype=torch.float32)
-        states_lst.append(torch_state)
-        state_value = value_v0(torch_state.to(device))
-        state_value_lst.append(state_value)
-        action_logits = policy_v0(torch_state.to(device))
-        action_probs = F.softmax(action_logits, dim=-1)
-        action_distribution = torch.distributions.Categorical(probs=action_probs)
-        selected_action = action_distribution.sample()
-        scalar_action = selected_action.item()
-        log_probs = action_distribution.log_prob(selected_action)
-        log_probs_lst.append(log_probs)
-        actions_lst.append(selected_action)
-        np_state, reward, terminated, truncated, info = env.step(scalar_action)
-        rewards_lst.append(reward)
-        if truncated or terminated:
+        old_torch_state = torch.as_tensor(old_np_state, dtype=torch.float32)
+        old_states_lst.append(old_torch_state)
+        old_state_value = value_v0(old_torch_state.to(device))
+        old_state_value_lst.append(old_state_value)
+        old_action_logits = policy_v0(old_torch_state.to(device))
+        old_action_probs = F.softmax(old_action_logits, dim=-1)
+        old_action_distribution = torch.distributions.Categorical(
+            probs=old_action_probs
+        )
+        old_selected_action = old_action_distribution.sample()
+        old_scalar_action = old_selected_action.item()
+        old_log_probs = old_action_distribution.log_prob(old_selected_action)
+        old_log_probs_lst.append(old_log_probs)
+        old_actions_lst.append(old_selected_action)
+        old_np_state, old_reward, old_terminated, old_truncated, old_info = env.step(
+            old_scalar_action
+        )
+        old_rewards_lst.append(old_reward)
+        if old_truncated or old_terminated:
             break
-    old_action_logits = old_policy_v0(torch.stack(states_lst))
-    old_action_probs = F.softmax(old_action_logits, dim=-1)
-    old_action_distribution = torch.distributions.Categorical(probs=old_action_probs)
-    old_log_probs_lst = old_action_distribution.log_prob(torch.stack(actions_lst))
+    old_rewards_lst = torch.tensor(old_rewards_lst, dtype=torch.float32, device=device)
+    old_returns = torch.zeros_like(old_rewards_lst, device=device)
+    old_running_return = 0
+    for t in reversed(range(len(old_rewards_lst))):
+        old_running_return = old_rewards_lst[t] + DISCOUNT * old_running_return
+        old_returns[t] = old_running_return
 
-    rewards_lst = torch.tensor(rewards_lst, dtype=torch.float32, device=device)
-    returns = torch.zeros_like(rewards_lst, device=device)
-    running_return = 0
-    for t in reversed(range(len(rewards_lst))):
-        running_return = rewards_lst[t] + DISCOUNT * running_return
-        returns[t] = running_return
-    advantage_lst = returns - torch.cat(state_value_lst, dim=-1)
+    old_advantage_lst = old_returns - torch.cat(old_state_value_lst, dim=-1)
+    new_action_logits = policy_v0(
+        torch.stack(old_states_lst).to(device).unsqueeze(0).repeat(4, 1, 1)
+    )
+    new_action_probs = F.softmax(new_action_logits, dim=-1)
+    print(new_action_probs.shape)
+    new_action_distributions = torch.distributions.Categorical(probs=new_action_probs)
+
+    new_log_probs_lst = new_action_distributions.log_prob(
+        torch.stack(old_actions_lst).repeat(4)
+    )
+    policy_loss = torch.minimum(
+        torch.exp(new_log_probs_lst - old_log_probs_lst) * old_advantage_lst,
+        torch.clip(
+            torch.exp(new_log_probs_lst - old_log_probs_lst), 1 - EPSILON, 1 + EPSILON
+        )
+        * old_advantage_lst,
+    )
+    print(policy_loss)
 
 
 for i in range(EPISODE_NUM // 50):
