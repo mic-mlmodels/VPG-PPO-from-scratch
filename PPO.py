22e737405159d466c1137c9a2a5a255328ac17f7
Michael <michaelzyx1@gmail.com>
2026-06-22 21:38:04 +1000

    failed attempt at ppo

--- PPO.py
+++ PPO.py
@@ -42,12 +42,16 @@ class ValueNN(nn.Module):
 
 
 policy_v0 = PolicyNN().to(device)
+old_policy_v0 = PolicyNN().to(device)
 value_v0 = ValueNN().to(device)
 policy_optimiser = torch.optim.AdamW(lr=3e-4, params=policy_v0.parameters())
 value_optimiser = torch.optim.AdamW(lr=3e-4, params=value_v0.parameters())
 episode_rewards = []
 mean_rewards = []
 for episode in range(EPISODE_NUM):
+    old_policy_v0.load_state_dict(policy_v0.state_dict())
+    for param in old_policy_v0.parameters():
+        param.requires_grad = False
     policy_optimiser.zero_grad()
     value_optimiser.zero_grad()
     states_lst = []
@@ -60,8 +64,8 @@ for episode in range(EPISODE_NUM):
     if episode % 100 == 0:
         print(episode)
     while True:
-        states_lst.append(np_state)
         torch_state = torch.as_tensor(np_state, dtype=torch.float32)
+        states_lst.append(torch_state)
         state_value = value_v0(torch_state.to(device))
         state_value_lst.append(state_value)
         action_logits = policy_v0(torch_state.to(device))
@@ -76,6 +80,11 @@ for episode in range(EPISODE_NUM):
         rewards_lst.append(reward)
         if truncated or terminated:
             break
+    old_action_logits = old_policy_v0(torch.stack(states_lst))
+    old_action_probs = F.softmax(old_action_logits, dim=-1)
+    old_action_distribution = torch.distributions.Categorical(probs=old_action_probs)
+    old_log_probs_lst = old_action_distribution.log_prob(torch.stack(actions_lst))
+
     rewards_lst = torch.tensor(rewards_lst, dtype=torch.float32, device=device)
     returns = torch.zeros_like(rewards_lst, device=device)
     running_return = 0
@@ -83,13 +92,8 @@ for episode in range(EPISODE_NUM):
         running_return = rewards_lst[t] + DISCOUNT * running_return
         returns[t] = running_return
     advantage_lst = returns - torch.cat(state_value_lst, dim=-1)
-    policy_loss = -torch.mean(advantage_lst.detach() * torch.stack(log_probs_lst))
-    episode_rewards.append(torch.sum(rewards_lst).item())
-    policy_loss.backward()
-    policy_optimiser.step()
-    value_loss = F.mse_loss(input=torch.cat(state_value_lst, dim=-1), target=returns)
-    value_loss.backward()
-    value_optimiser.step()
+
+
 for i in range(EPISODE_NUM // 50):
     mean_rewards.append(np.mean(episode_rewards[i * 50 : (i + 1) * 50]))
 print(mean_rewards)
